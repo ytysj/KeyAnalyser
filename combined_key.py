43108edd1882cf8b7db5ab0ae811a04a7c8c774b
chyou <zxcv1w@hotmail.com>
2023-09-04 00:46:32 +0800

    update: use keycode type in combined_key.py

--- combined_key.py
+++ combined_key.py
@@ -1,12 +1,20 @@
 from pynput import keyboard
+from pynput.keyboard import KeyCode
 
 #list all combined key
-dict_combined_key = {
+raw_dict_combined_key = {
+    "\x13":["ctrl", 's'], \
     "\x1a":["ctrl", 'z'], "\x18":["ctrl", 'x'], "\x03":["ctrl", 'c'], "\x16":["ctrl", 'v'], \
     "\x02":["ctrl", 'b'], "\x0e":["ctrl", 'n'], "\x1b":["ctrl", '['], "\x1d":["ctrl", ']'], \
 }
+dict_combined_key = {}
+
+for key, value in raw_dict_combined_key.items():
+    new_key = KeyCode.from_char(key)
+    dict_combined_key[new_key] = value
 
 def unpack_combined_key(key):
+    # print("unpack_combined_key", key)
     if key in dict_combined_key:
         return dict_combined_key[key]
     else:
@@ -18,7 +26,7 @@ if __name__ == "__main__":
     print(rst)
     rst = unpack_combined_key("\x18")
     print(rst)
-    rst = unpack_combined_key("\x1b")
+    rst = unpack_combined_key("\x03")
     print(rst)
     rst = unpack_combined_key("\x99")
     print(rst)
